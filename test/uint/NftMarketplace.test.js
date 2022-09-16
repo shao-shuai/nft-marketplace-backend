0111ad65b4c219688c81a6ba6c97e459f5c10e00
shao-shuai <shuai-shao@outlook.com>
2022-09-15 22:52:10 -0700

    updated uint test

--- test/uint/NftMarketplace.test.js
+++ test/uint/NftMarketplace.test.js
@@ -1,3 +1,40 @@
-const { assert, expect } = require("chain")
+const { assert, expect } = require("chai")
 const { network, deployments, ethers } = require("hardhat")
-const { developmentChains } = require("../helper-hardhat-config")
+const { developmentChains } = require("../../helper-hardhat-config")
+
+!developmentChains.includes(network.name)
+    ? describe.skip
+    : describe("Nft Marketplace Uint Tests", function () {
+          let nftMarketplace, nftMarketplaceContract, basicNft, basicNftContract
+          const PRICE = ethers.utils.parseEther("0.1")
+          const TOKEN_ID = 0
+
+          beforeEach(async () => {
+              accounts = await ethers.getSigners()
+              deployer = accounts[0]
+              user = accounts[1]
+              await deployments.fixture(["all"])
+              nftMarketplaceContract = await ethers.getContract("NftMarketplace")
+              nftMarketplace = nftMarketplaceContract.connect(deployer)
+              basicNftContract = await ethers.getContract("BasicNft")
+              basicNft = basicNftContract.connect(deployer)
+              await basicNft.mintNft()
+              await basicNft.approve(nftMarketplaceContract.address, TOKEN_ID)
+          })
+
+          describe("listItem", function () {
+              it("emits an event after listing an item", async function () {
+                  expect(await nftMarketplace.listItem(basicNft.address, TOKEN_ID, PRICE)).to.emit(
+                      "ItemListed"
+                  )
+              })
+
+              it("exclude items that haven't been listed", async function () {
+                  await nftMarketplace.listItem(basicNft.address, TOKEN_ID, PRICE)
+                  const error = `NftMarketplace_AlreadyListed("${basicNft.address}", ${TOKEN_ID})`
+                  await expect(
+                      nftMarketplace.listItem(basicNft.address, TOKEN_ID, PRICE)
+                  ).to.be.revertedWith(error)
+              })
+          })
+      })
